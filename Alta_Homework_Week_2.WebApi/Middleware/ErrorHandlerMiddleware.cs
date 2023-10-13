c7089539e22d18e0fd3b3fece193f2bb424b4e5a
Zigguraptor <cobaltum8@gmail.com>
2023-10-14 00:13:39 +0400

    added ErrorHandlerMiddleware.cs

---
+++ Alta_Homework_Week_2.WebApi/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace Alta_Homework_Week_2.WebApi.Middleware;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+    public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        _logger.LogError(exception.ToString());
+        context.Response.Clear();
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Internal Server Error");
+    }
+}
