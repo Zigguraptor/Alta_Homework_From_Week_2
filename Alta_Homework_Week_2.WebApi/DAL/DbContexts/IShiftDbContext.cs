7c8515f70e8eb29ba600873db9597bcf34574371
Zigguraptor <cobaltum8@gmail.com>
2023-10-13 18:15:29 +0400

    added ShiftDbContext

---
+++ Alta_Homework_Week_2.WebApi/DAL/DbContexts/IShiftDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Alta_Homework_Week_2.WebApi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alta_Homework_Week_2.WebApi.DAL.DbContexts
+{
+    public interface IShiftDbContext
+    {
+        DbSet<JobTitle> JobTitles { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<ShiftRecord> ShiftRecords { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Alta_Homework_Week_2.WebApi/DAL/DbContexts/ShiftDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Alta_Homework_Week_2.WebApi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alta_Homework_Week_2.WebApi.DAL.DbContexts
+{
+    public sealed class ShiftDbContext : DbContext, IShiftDbContext
+    {
+        public DbSet<JobTitle> JobTitles { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<ShiftRecord> ShiftRecords { get; set; } = null!;
+
+        public ShiftDbContext(DbContextOptions<ShiftDbContext> options) : base(options) => Database.EnsureCreated();
+    }
+}
