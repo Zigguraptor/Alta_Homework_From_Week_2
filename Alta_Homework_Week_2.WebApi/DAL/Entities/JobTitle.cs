d73785ead0d663bd3023103280498ecfc858f18f
Zigguraptor <cobaltum8@gmail.com>
2023-10-13 18:11:47 +0400

    added entity JobTitle

---
+++ Alta_Homework_Week_2.WebApi/DAL/Entities/JobTitle.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alta_Homework_Week_2.WebApi.DAL.Entities
+{
+    public class JobTitle
+    {
+        [Key] public required string Title { get; set; }
+    }
+}
