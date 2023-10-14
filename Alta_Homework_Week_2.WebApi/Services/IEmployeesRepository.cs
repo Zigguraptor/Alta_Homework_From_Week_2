824488250324c7b91d6e6aa6180e55ccff7b6bd8
Zigguraptor <cobaltum8@gmail.com>
2023-10-14 23:09:55 +0400

    added IEmployeesRepository.cs

---
+++ Alta_Homework_Week_2.WebApi/Services/IEmployeesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Alta_Homework_Week_2.WebApi.DTOs;
+
+namespace Alta_Homework_Week_2.WebApi.Services;
+
+public interface IEmployeesRepository
+{
+    public Task<List<EmployeeVm>> GetEmployees();
+    public Task<EmployeeVm> GetEmployee(int id);
+    public Task AddNewEmployee(CreateEmployeeDto createEmployeeDto);
+    public Task DeleteEmployee(int id);
+    public Task UpdateEmployee(UpdateEmployeeDto updateEmployeeDto);
+}
