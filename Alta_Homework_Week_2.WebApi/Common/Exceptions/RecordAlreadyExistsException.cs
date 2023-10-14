6f0f3bc0cdedb0fbefb217688bca2825d7c28d33
Zigguraptor <cobaltum8@gmail.com>
2023-10-14 22:54:55 +0400

    added RecordAlreadyExistsException.cs

---
+++ Alta_Homework_Week_2.WebApi/Common/Exceptions/RecordAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿namespace Alta_Homework_Week_2.WebApi.Common.Exceptions;
+
+public class RecordAlreadyExistsException : Exception
+{
+    public RecordAlreadyExistsException()
+    {
+    }
+
+    public RecordAlreadyExistsException(string? message) : base(message)
+    {
+    }
+
+    public RecordAlreadyExistsException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
