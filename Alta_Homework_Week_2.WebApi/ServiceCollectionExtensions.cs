bf319d55c3e217da734d9396244aa12af5923cd8
Zigguraptor <cobaltum8@gmail.com>
2023-10-14 23:00:18 +0400

    added ServiceCollectionExtensions.cs

--- Alta_Homework_Week_2.WebApi/ServiceCollectionExtensions.cs
+++ Alta_Homework_Week_2.WebApi/ServiceCollectionExtensions.cs
@@ -1,10 +1,22 @@
-﻿using Alta_Homework_Week_2.WebApi.DAL.DbContexts;
+﻿using System.Reflection;
+using Alta_Homework_Week_2.WebApi.Common.Mappings;
+using Alta_Homework_Week_2.WebApi.DAL.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
 namespace Alta_Homework_Week_2.WebApi;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddAutoMappingProfiles(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+        });
+
+        return serviceCollection;
+    }
+
     public static IServiceCollection AddDbContexts(this IServiceCollection serviceCollection,
         ConfigurationManager configurationManager)
     {
