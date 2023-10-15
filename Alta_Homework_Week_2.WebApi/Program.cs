8c658aee0062209e7a16676f96afcdac52f4f28f
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 15:45:10 +0400

    added nLog to pipeline

--- Alta_Homework_Week_2.WebApi/Program.cs
+++ Alta_Homework_Week_2.WebApi/Program.cs
@@ -2,32 +2,56 @@ using Alta_Homework_Week_2.WebApi;
 using Alta_Homework_Week_2.WebApi.Common.Services;
 using Alta_Homework_Week_2.WebApi.Middleware;
 using Alta_Homework_Week_2.WebApi.Services;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-builder.Services.AddAutoMappingProfiles();
-builder.Services.AddDbContexts(builder.Configuration);
-
-builder.Services.AddSingleton<IDateTimeService, CommonDateTimeUtcService>();
-
-builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
-builder.Services.AddScoped<IJobsRepository, JobsRepository>();
-builder.Services.AddScoped<IShiftsRepository, ShiftsRepository>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-app.UseMiddleware<ErrorHandlerMiddleware>();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+// logging
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+// DBs and mapping
+    builder.Services.AddAutoMappingProfiles();
+    builder.Services.AddDbContexts(builder.Configuration);
+
+// custom services
+    builder.Services.AddSingleton<IDateTimeService, CommonDateTimeUtcService>();
+
+    builder.Services.AddScoped<IEmployeesRepository, EmployeesRepository>();
+    builder.Services.AddScoped<IJobsRepository, JobsRepository>();
+    builder.Services.AddScoped<IShiftsRepository, ShiftsRepository>();
+
+// other
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
