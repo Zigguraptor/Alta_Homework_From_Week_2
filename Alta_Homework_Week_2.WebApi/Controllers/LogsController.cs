4cc0818615771062821af6272f9dc2a42c18d8c5
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 20:02:38 +0400

    added LogsController.cs

---
+++ Alta_Homework_Week_2.WebApi/Controllers/LogsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alta_Homework_Week_2.WebApi.Controllers;
+
+[ApiController]
+[Route("logs")]
+[ApiExplorerSettings(IgnoreApi = true)]
+public class LogsController : ControllerBase
+{
+    [HttpGet("{fileName}")]
+    public async Task<IActionResult> Index(string fileName)
+    {
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", fileName);
+        await using var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        return File(stream, "application/octet-stream", fileName);
+    }
+}
