68fb062e7a152db60d4f0473cc4178e5f2a4c9e9
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 20:02:07 +0400

    added extension for WebApplication AddLogFilesDirectoryBrowser

---
+++ Alta_Homework_Week_2.WebApi/WebApplicationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace Alta_Homework_Week_2.WebApi;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication AddLogFilesDirectoryBrowser(this WebApplication app)
+    {
+        app.UseDirectoryBrowser(new DirectoryBrowserOptions
+        {
+            FileProvider =
+                new PhysicalFileProvider(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs")),
+            RequestPath = "/logs"
+        });
+
+        return app;
+    }
+}
