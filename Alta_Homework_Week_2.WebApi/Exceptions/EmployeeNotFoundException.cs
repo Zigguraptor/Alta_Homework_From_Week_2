24c90eb084f99426711cee3c595e40b91e3820b2
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 13:57:11 +0400

    added EmployeeNotFoundException.cs

---
+++ Alta_Homework_Week_2.WebApi/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace Alta_Homework_Week_2.WebApi.Exceptions;
+
+public class EmployeeNotFoundException : Exception
+{
+    public EmployeeNotFoundException()
+    {
+    }
+
+    public EmployeeNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public EmployeeNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
