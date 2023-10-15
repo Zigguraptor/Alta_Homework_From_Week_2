b8a5a81d088081439629ad8a0a93d5eabe7a09e1
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 12:30:40 +0400

    added JobsRepository.cs

---
+++ Alta_Homework_Week_2.WebApi/Services/JobsRepository.cs
@@ -0,0 +1,37 @@
+﻿using Alta_Homework_Week_2.WebApi.Common.Exceptions;
+using Alta_Homework_Week_2.WebApi.DAL.DbContexts;
+using Alta_Homework_Week_2.WebApi.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alta_Homework_Week_2.WebApi.Services;
+
+public class JobsRepository : IJobsRepository
+{
+    private readonly IEmployeesShiftDbContext _employeesShiftDbContext;
+
+    public JobsRepository(IEmployeesShiftDbContext employeesShiftDbContext) =>
+        _employeesShiftDbContext = employeesShiftDbContext;
+
+    public Task<List<string>> GetJobTitles() =>
+        _employeesShiftDbContext.JobTitles.Select(j => j.Title).ToListAsync();
+
+    public async Task AddNewJobTitle(string jobTitle)
+    {
+        if (await _employeesShiftDbContext.JobTitles.FindAsync(jobTitle) != null)
+            throw new RecordAlreadyExistsException();
+
+        _employeesShiftDbContext.JobTitles.Add(new JobTitleEntity { Title = jobTitle });
+
+        await _employeesShiftDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteJobAsync(string jobTitle)
+    {
+        var localJobTitle = await _employeesShiftDbContext.JobTitles.FindAsync(jobTitle);
+        if (localJobTitle == null)
+            throw new KeyNotFoundException();
+
+        _employeesShiftDbContext.JobTitles.Remove(localJobTitle);
+        await _employeesShiftDbContext.SaveChangesAsync();
+    }
+}
