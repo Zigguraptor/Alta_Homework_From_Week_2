928b68f512eca602636a200f649c53e75315716b
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 13:57:23 +0400

    added ShiftsRepository.cs

---
+++ Alta_Homework_Week_2.WebApi/Services/ShiftsRepository.cs
@@ -0,0 +1,59 @@
+﻿using Alta_Homework_Week_2.WebApi.Common.Exceptions;
+using Alta_Homework_Week_2.WebApi.Common.Services;
+using Alta_Homework_Week_2.WebApi.DAL.DbContexts;
+using Alta_Homework_Week_2.WebApi.DAL.Entities;
+using Alta_Homework_Week_2.WebApi.Exceptions;
+
+namespace Alta_Homework_Week_2.WebApi.Services;
+
+public class ShiftsRepository : IShiftsRepository
+{
+    private readonly IEmployeesShiftDbContext _employeesShiftDbContext;
+    private readonly IDateTimeService _dateTimeService;
+
+    public ShiftsRepository(IEmployeesShiftDbContext employeesShiftDbContext, IDateTimeService dateTimeService)
+    {
+        _employeesShiftDbContext = employeesShiftDbContext;
+        _dateTimeService = dateTimeService;
+    }
+
+    public async Task StartShift(int employeeId)
+    {
+        if (!_employeesShiftDbContext.Employees.Any(e => e.Id == employeeId))
+            throw new EmployeeNotFoundException();
+
+        var shiftRecord = _employeesShiftDbContext.ShiftRecords
+            .Where(sr => sr.EmployeeId == employeeId)
+            .OrderByDescending(sr => sr.StartTime)
+            .FirstOrDefault();
+
+        if (shiftRecord is { EndTime: null })
+            throw new RecordAlreadyExistsException();
+
+        var shiftRecordEntity = new ShiftRecordEntity
+        {
+            StartTime = _dateTimeService.Now,
+            EndTime = null,
+            EmployeeId = employeeId
+        };
+
+        _employeesShiftDbContext.ShiftRecords.Add(shiftRecordEntity);
+        await _employeesShiftDbContext.SaveChangesAsync();
+    }
+
+    public Task EndShift(int employeeId)
+    {
+        if (!_employeesShiftDbContext.Employees.Any(e => e.Id == employeeId))
+            throw new EmployeeNotFoundException();
+
+        var shiftRecord = _employeesShiftDbContext.ShiftRecords
+            .FirstOrDefault(shift => shift.EmployeeId == employeeId && shift.EndTime == null);
+
+        if (shiftRecord == null)
+            throw new RecordNotFoundException();
+
+        shiftRecord.EndTime = _dateTimeService.Now;
+
+        return _employeesShiftDbContext.SaveChangesAsync();
+    }
+}
