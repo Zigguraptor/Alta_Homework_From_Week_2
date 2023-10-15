bbe791467895fd525a2244c59255a82bbb8600a7
Zigguraptor <cobaltum8@gmail.com>
2023-10-15 21:30:23 +0400

    fixed disposing

--- Alta_Homework_Week_2.WebApi/Controllers/LogsController.cs
+++ Alta_Homework_Week_2.WebApi/Controllers/LogsController.cs
@@ -8,10 +8,10 @@ namespace Alta_Homework_Week_2.WebApi.Controllers;
 public class LogsController : ControllerBase
 {
     [HttpGet("{fileName}")]
-    public async Task<IActionResult> Index(string fileName)
+    public IActionResult Index(string fileName)
     {
         var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", fileName);
-        await using var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         return File(stream, "application/octet-stream", fileName);
     }
 }
